5d1626e1b9ddc93e49acab243d52881e0b1f5e13
Leaf Games <leafgames@gmx.net>
2025-06-29 17:48:22 +0200

    favicon + smooth scrolling

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const jsConfetti = new JSConfetti()
+const jsConfetti = new JSConfetti();
 
 function confetti() {
-    jsConfetti.addConfetti()
+  jsConfetti.addConfetti();
 }
+
+document
+  .querySelector(".header-text-block a")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("projekte").scrollIntoView({ behavior: "smooth" });
+  });
+
+document.querySelector(".signature a").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
